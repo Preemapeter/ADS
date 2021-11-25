a761fe6bfb10cbe281e0ee3aa09a0d6dd3235fb9
Preemapeter <preemapeter17@gmail.com>
2021-11-25 18:33:10 +0530

    second  commit

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("\nenter thr First number");
+	scanf("%d",&a);
+	printf("\nenter the second number");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("\nsum=%d",sum);
+	return(0);
+}
